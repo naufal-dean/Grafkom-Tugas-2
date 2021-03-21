eb85ed83251f9228bd3a6db27a743926f18116f5
naufal-dean <13518123@std.stei.itb.ac.id>
2021-03-22 00:01:40 +0700

    chore: add vec utility class

---
+++ src/util/vector.ts
@@ -0,0 +1,68 @@
+class vec {
+  static add = (v1: number[], v2: number[]) => {
+    if (v1.length != v2.length) {
+      throw "vec.add: vector length must be equal";
+    }
+
+    let res = [];
+    for (let i = 0; i < v1.length; i++) {
+      res.push(v1[i] + v2[i]);
+    }
+    return res;
+  }
+
+  static sub = (v1: number[], v2: number[]) => {
+    if (v1.length != v2.length) {
+      throw "vec.sub: vector length must be equal";
+    }
+
+    let res = [];
+    for (let i = 0; i < v1.length; i++) {
+      res.push(v1[i] - v2[i]);
+    }
+    return res;
+  }
+
+  static dot = (v1: number[], v2: number[]) => {
+    if (v1.length != v2.length) {
+      throw "vec.dot: vector length must be equal";
+    }
+
+    let res = 0;
+    for (let i = 0; i < v1.length; i++) {
+      res += v1[i] * v2[i];
+    }
+    return res;
+  }
+
+  static cross = (v1: number[], v2: number[]) => {
+    // Notes: only support vector with length == 3
+
+    if (v1.length != 3 ||  v2.length != 3) {
+      throw "vec.cross: vector length must equal to 3";
+    }
+
+    // prettier-ignore
+    return [
+      v1[1] * v2[2] - v1[2] * v2[1],
+      v1[2] * v2[0] - v1[0] * v2[2],
+      v1[0] * v2[1] - v1[1] * v2[0],
+    ];
+  }
+
+  static length = (v: number[]) => {
+    return Math.sqrt(vec.dot(v));
+  }
+
+  static normalize = (v: number[]) => {
+    // Notes: the input array is not preserved, it is assumed that the length of v is not 0
+
+    const vLength = vec.length(v);
+    for (let i = 0; i < v.length; i++) {
+      v[i] /= vLength;
+    }
+    return v;
+  }
+}
+
+export {vec};
