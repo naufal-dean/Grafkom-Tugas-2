4cbdb4e0e702207555e4d2a8a0191e5a5b9360ab
naufal-dean <13518123@std.stei.itb.ac.id>
2021-03-24 19:14:04 +0700

    style: remove unused import

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import App from "./app";
 import Cube from "./shapes/cube";
 import Block from "./shapes/block";
-import {triangularPrism} from "./shapes/initialPoints";
 import TriangularPrism from "./shapes/triangularPrism";
 import Shape from "./shapes/shape";
 
